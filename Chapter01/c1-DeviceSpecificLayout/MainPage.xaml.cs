6ab21ca09b8f20874a42eef17bd4b978161a6148
ghostNoté <selcukbilgen@hotmail.com>
2025-04-27 23:04:18 +0300

    Implementing device-specific layouts

--- Chapter01/c1-DeviceSpecificLayout/MainPage.xaml.cs
+++ Chapter01/c1-DeviceSpecificLayout/MainPage.xaml.cs
@@ -5,5 +5,13 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
+        if (DeviceInfo.Current.Idiom == DeviceIdiom.Desktop)
+        {
+            grid1.Height = CalculateDesktopHeight();
+        }
+        else if (DeviceInfo.Current.Idiom == DeviceIdiom.Phone)
+        {
+            grid1.Height = CalculateMobileHeight();
+        }
     }
 }
