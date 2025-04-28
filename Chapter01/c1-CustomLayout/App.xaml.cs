f69a51bc4f5e3bbed4112d85a5fd2dc4e0e5963a
ghostNoté <selcukbilgen@hotmail.com>
2025-04-28 23:58:34 +0300

    Add CustomLayout project to solution

---
+++ Chapter01/c1-CustomLayout/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace c1_CustomLayout;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
+    }
+}
---
+++ Chapter01/c1-CustomLayout/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Maui.Layouts;
+
+namespace c1_CustomLayout;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
+
+public class CircularLayout : Layout
+{
+    public static readonly BindableProperty RadiusProperty = BindableProperty.Create("Radius", typeof(double), typeof(CircularLayout));
+
+    public double Radius
+    {
+        get { return (double)GetValue(RadiusProperty); }
+        set { SetValue(RadiusProperty, value); }
+    }
+
+    protected override ILayoutManager CreateLayoutManager()
+    {
+        return new CircularLayoutManager(this);
+    }
+}
+
+public class CircularLayoutManager : ILayoutManager
+{
+    private readonly CircularLayout parentLayout;
+
+    public CircularLayoutManager(CircularLayout layout)
+    {
+        parentLayout = layout;
+    }
+
+    public Size Measure(double widthConstraint, double heightConstraint)
+    {
+        var radius = parentLayout.Radius;
+        for (var n = 0; n < parentLayout.Count; n++)
+        {
+            var child = parentLayout[n];
+            if (child.Visibility == Visibility.Collapsed) continue;
+            child.Measure(double.PositiveInfinity, double.PositiveInfinity);
+        }
+
+        return new Size(parentLayout.WidthRequest, parentLayout.HeightRequest);
+    }
+
+    public Size ArrangeChildren(Rect bounds)
+    {
+        var radius = parentLayout.Radius;
+        var angleStep = Math.PI * 2 / parentLayout.Count;
+        for (var i = 0; i < parentLayout.Count; i++)
+        {
+            var child = parentLayout[i];
+            if (child.Visibility == Visibility.Collapsed) continue;
+            child.Arrange(new Rect(
+                radius * Math.Cos(angleStep * i) + radius,
+                radius * Math.Sin(angleStep * i) + radius,
+                child.DesiredSize.Width,
+                child.DesiredSize.Height));
+        }
+
+        return new Size(parentLayout.WidthRequest, parentLayout.HeightRequest);
+    }
+}
