0c43f8f1b98672459f9bba7965f477d482c67d95
ghostNoté <selcukbilgen@hotmail.com>
2025-04-28 16:19:21 +0300

    Implementing layouts with dynamic orientation

--- Chapter01/c1-DeviceSpecificLayout/MainPage.xaml.cs
+++ Chapter01/c1-DeviceSpecificLayout/MainPage.xaml.cs
@@ -5,13 +5,13 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
-        if (DeviceInfo.Current.Idiom == DeviceIdiom.Desktop)
+        /*if (DeviceInfo.Current.Idiom == DeviceIdiom.Desktop)
         {
             grid1.Height = CalculateDesktopHeight();
         }
         else if (DeviceInfo.Current.Idiom == DeviceIdiom.Phone)
         {
             grid1.Height = CalculateMobileHeight();
-        }
+        }*/
     }
 }
---
+++ Chapter01/c1-OrientationSpecificSettings/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace c1_OrientationSpecificSettings;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
