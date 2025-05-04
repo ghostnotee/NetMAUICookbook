99fdcdef720d8815fe0f07e11905c38d3297fe74
ghostNoté <selcukbilgen@hotmail.com>
2025-05-04 16:27:19 +0300

    Add c2-CollectionInitialization project to solution

---
+++ Chapter02/c2-CollectionInitialization/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace c2_CollectionInitialization;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
