a9c266094d26d05b17fd248af5fcdb0dd47cbaa6
ghostNoté <selcukbilgen@hotmail.com>
2025-05-05 16:50:16 +0300

    Add c2-UiAndViewModelInteraction project to solution

---
+++ Chapter02/c2-UiAndViewModelInteraction/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace c2_UiAndViewModelInteraction;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Chapter02/c2-UiAndViewModelInteraction/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace c2_UiAndViewModelInteraction;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
