504054a32fdb04f6d9d894f8e6b4326b11aba45e
ghostNoté <selcukbilgen@hotmail.com>
2025-05-07 01:28:40 +0300

    Add c2-ViewModelCommunication project to solution

---
+++ Chapter02/c2-ViewModelCommunication/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace c2_ViewModelCommunication;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new AppShell());
+    }
+}
---
+++ Chapter02/c2-ViewModelCommunication/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace c2_ViewModelCommunication;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
