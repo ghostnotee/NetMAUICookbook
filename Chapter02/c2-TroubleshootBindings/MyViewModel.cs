610e907f24305899feaff18a20a408128ad1290d
ghostNoté <selcukbilgen@hotmail.com>
2025-05-09 14:01:49 +0300

    Refactor MainPage to use ViewModel and CollectionView for customer data display

---
+++ Chapter02/c2-TroubleshootBindings/MyViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace c2_TroubleshootBindings;
+
+public partial class MyViewModel : ObservableObject
+{
+    [ObservableProperty] private ObservableCollection<Customer> _customers;
+
+    public MyViewModel()
+    {
+        Customers = new ObservableCollection<Customer>();
+        for (var i = 1; i < 30; i++) Customers.Add(new Customer { Id = i, Name = "Name" + i });
+    }
+
+    [RelayCommand]
+    public void DeleteCustomer(Customer customer)
+    {
+        Customers.Remove(customer);
+    }
+}
+
+public class Customer
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+}
