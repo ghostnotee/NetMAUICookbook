575fd7fbe82f29286ad70af3e320862e592cc604
ghostNoté <selcukbilgen@hotmail.com>
2025-05-10 16:01:28 +0300

    Initialize new .NET MAUI application structure with essential files and configurations

---
+++ Chapter03/c3_AttachedBehavior/c3_AttachedBehavior/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace c3_AttachedBehavior;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
