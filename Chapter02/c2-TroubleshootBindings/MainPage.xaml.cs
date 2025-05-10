f3c6d2891c768e0ef073568d0117e4f9d7dd7c49
ghostNoté <selcukbilgen@hotmail.com>
2025-05-10 15:11:09 +0300

    Refactor MainPage to improve data binding and add a DummyConverter for value conversion

--- Chapter02/c2-TroubleshootBindings/MainPage.xaml.cs
+++ Chapter02/c2-TroubleshootBindings/MainPage.xaml.cs
@@ -1,9 +1,24 @@
-﻿namespace c2_TroubleshootBindings;
+﻿using System.Globalization;
+
+namespace c2_TroubleshootBindings;
 
 public partial class MainPage
 {
     public MainPage()
     {
         InitializeComponent();
+    }
+}
+
+public class DummyConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return null;
     }
 }
