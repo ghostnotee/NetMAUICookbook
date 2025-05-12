b82e990864ee44d77a66e46bf15abac60d9a2bef
ghostNoté <selcukbilgen@hotmail.com>
2025-05-12 13:33:07 +0300

    Add AttentionAnimation and button with animation behavior to MainPage

--- Chapter03/c3-CustomAnimations/MainPage.xaml.cs
+++ Chapter03/c3-CustomAnimations/MainPage.xaml.cs
@@ -1,10 +1,23 @@
-﻿namespace c3_CustomAnimations;
+﻿using CommunityToolkit.Maui.Animations;
+
+namespace c3_CustomAnimations;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 }
 
+internal class AttentionAnimation : BaseAnimation
+{
+    public override async Task Animate(VisualElement view, CancellationToken token = default)
+    {
+        for (var i = 0; i < 6; i++)
+        {
+            await view.FadeTo(0.5, Length, Easing);
+            await view.FadeTo(1, Length, Easing);
+        }
+    }
+}
