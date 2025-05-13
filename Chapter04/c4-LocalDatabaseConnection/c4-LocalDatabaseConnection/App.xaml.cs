d3e12ff17dcefef512b133017905762788ecae3a
ghostNoté <selcukbilgen@hotmail.com>
2025-05-13 23:55:50 +0300

    Initialize .NET MAUI project with essential application structure and resources

---
+++ Chapter04/c4-LocalDatabaseConnection/c4-LocalDatabaseConnection/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace c4_LocalDatabaseConnection;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
