57360edb450d2f7a9292dfbe1ff37d0d38bd65ba
ghostNoté <selcukbilgen@hotmail.com>
2025-05-13 14:26:05 +0300

    Add theme management and update MainPage layout with theme picker

--- Chapter03/c3-ThemedImagesAndStatusBar/MainPage.xaml.cs
+++ Chapter03/c3-ThemedImagesAndStatusBar/MainPage.xaml.cs
@@ -1,24 +1,46 @@
-﻿namespace c3_ThemedImagesAndStatusBar;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace c3_ThemedImagesAndStatusBar;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 }
 
+public class ThemeInfo
+{
+    public static ThemeInfo System = new(AppTheme.Unspecified, "System");
+    public static ThemeInfo Light = new(AppTheme.Light, "Light");
+    public static ThemeInfo Dark = new(AppTheme.Dark, "Dark");
+
+    public ThemeInfo(AppTheme theme, string caption)
+    {
+        AppTheme = theme;
+        Caption = caption;
+    }
+
+    public AppTheme AppTheme { get; }
+    public string Caption { get; }
+}
+
+public partial class ThemeSettings : ObservableObject
+{
+    [ObservableProperty] public ThemeInfo selectedTheme = ThemesList.First();
+
+    public static List<ThemeInfo> ThemesList { get; } = new()
+    {
+        ThemeInfo.System,
+        ThemeInfo.Light,
+        ThemeInfo.Dark
+    };
+
+    public static ThemeSettings Current { get; } = new();
+
+    partial void OnSelectedThemeChanged(ThemeInfo oldValue, ThemeInfo newValue)
+    {
+        Application.Current.UserAppTheme = newValue.AppTheme;
+    }
+}
