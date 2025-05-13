59bade5bf3fe90fc4b75ec871df3b3e93e7db506
ghostNoté <selcukbilgen@hotmail.com>
2025-05-13 14:21:08 +0300

    Initialize .NET MAUI project with essential files, configurations, and styles

---
+++ Chapter03/c3-ThemedImagesAndStatusBar/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace c3_ThemedImagesAndStatusBar;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Chapter03/c3-ThemedImagesAndStatusBar/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace c3_ThemedImagesAndStatusBar;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
