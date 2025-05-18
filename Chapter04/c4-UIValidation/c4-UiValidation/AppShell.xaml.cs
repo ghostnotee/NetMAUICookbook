b4469513b597f8e8886f5180f9a83eb11a220f17
ghostNoté <selcukbilgen@hotmail.com>
2025-05-18 16:14:49 +0300

    Initialize .NET MAUI application structure with essential files and configurations

---
+++ Chapter04/c4-UIValidation/c4-UiValidation/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace c4_UiValidation;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Chapter04/c4-UIValidation/c4-UiValidation/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace c4_UiValidation;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
