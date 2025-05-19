210f70dfaf4108a3e8b07b8fe6aea1e70c30587e
ghostNoté <selcukbilgen@hotmail.com>
2025-05-19 13:55:37 +0300

    Initialize .NET MAUI application structure with essential files and configurations

---
+++ Chapter04/c4-DataCaching/c4-DataCaching/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace c4_DataCaching;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Chapter04/c4-DataCaching/c4-DataCaching/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace c4_DataCaching;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
