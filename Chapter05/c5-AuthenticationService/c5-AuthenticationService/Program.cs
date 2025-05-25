50e08da82bc04792547f141851f06a78f5771809
ghostNoté <selcukbilgen@hotmail.com>
2025-05-25 15:34:06 +0300

    Add Entity Framework Core and Identity support to c5-AuthenticationService

--- Chapter05/c5-AuthenticationService/c5-AuthenticationService/Program.cs
+++ Chapter05/c5-AuthenticationService/c5-AuthenticationService/Program.cs
@@ -1,9 +1,22 @@
+using c5_AuthenticationService;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication()
+    .AddBearerToken(IdentityConstants.BearerScheme);
+builder.Services.AddIdentityCore<User>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddApiEndpoints()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(@"Data Source=mydatabase.db"));
 
 var app = builder.Build();
 
@@ -16,5 +29,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.MapIdentityApi<User>();
 app.Run();
