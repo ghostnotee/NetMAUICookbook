adb77f0a45c0e94bc43ba3820205433c0047e9e2
ghostNoté <selcukbilgen@hotmail.com>
2025-05-28 23:59:13 +0300

    Implement initial setup for c5 Authentication Service with user management and role assignment

---
+++ Chapter05/c5-RoleBasedAccessPart2/c5-AuthenticationService/Program.cs
@@ -0,0 +1,144 @@
+using System.Security.Claims;
+using c5_AuthenticationService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "WebAPI API",
+        Version = "v1"
+    });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.Http,
+        Name = "Bearer",
+        Scheme = "bearer",
+        BearerFormat = "Bearer",
+        In = ParameterLocation.Header
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            []
+        }
+    });
+});
+
+builder.Services
+    .AddAuthentication()
+    .AddBearerToken(IdentityConstants.BearerScheme);
+
+builder.Services
+    .AddIdentityCore<User>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddApiEndpoints()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(@"Data Source=mydatabase.db"));
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminPolicy", policy => policy.RequireRole("Admin"));
+    options.AddPolicy("UserPolicy", policy => policy.RequireRole("User"));
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapIdentityApi<User>();
+
+app.MapGet("/users", async (UserManager<User> userManager) =>
+{
+    var users = await userManager.Users
+        .Select(u => new
+        {
+            u.Email,
+            u.BirthDate
+        })
+        .ToListAsync();
+
+    return Results.Ok(users);
+}).RequireAuthorization();
+
+app.MapDelete("/users/delete/{email}", async (
+    string email,
+    UserManager<User> userManager,
+    HttpContext httpContext) =>
+{
+    var user = await userManager.FindByEmailAsync(email);
+    if (user == null) return Results.NotFound();
+    var result = await userManager.DeleteAsync(user);
+    if (result.Succeeded) return Results.Ok();
+    return Results.Problem("Failed to delete the user.");
+}).RequireAuthorization("AdminPolicy");
+
+app.MapGet("/users/candelete", async (
+    HttpContext httpContext,
+    IAuthorizationService authorizationService) =>
+{
+    var authResult = await authorizationService.AuthorizeAsync(httpContext.User, "AdminPolicy");
+    return Results.Ok(authResult);
+});
+
+app.MapGet("/me", async (ClaimsPrincipal principal, UserManager<User> userManager) =>
+{
+    var userEmail = principal.Claims.First(c => c.Type == ClaimTypes.Email).Value;
+    var currentUser = await userManager.FindByEmailAsync(userEmail);
+    return Results.Ok(new
+    {
+        currentUser.Email,
+        currentUser.BirthDate
+    });
+}).RequireAuthorization();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetRequiredService<ApplicationDbContext>();
+
+    dbContext.Database.EnsureCreated();
+    var userManager = services.GetRequiredService<UserManager<User>>();
+    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+    await userManager.CreateUserWithRoleAsync(
+        roleManager,
+        "admin@cookbook.com",
+        "123Password123!",
+        new DateOnly(1991, 4, 20),
+        "Admin");
+    
+    for (var i = 1; i < 10; i++)
+        await userManager.CreateUserWithRoleAsync(
+            roleManager,
+            $"user{i}@cookbook.com",
+            "123Password123!",
+            new DateOnly(2000, 1, i), "User");
+}
+
+app.Run();
