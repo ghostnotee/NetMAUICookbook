038ba0e77d063a6ff8e6f7acb6fa5b9dbc915c84
ghostNoté <selcukbilgen@hotmail.com>
2025-05-31 17:07:57 +0300

    Refactor MainPage layout and bind to ViewModel; update project dependencies

---
+++ Chapter06/c6-OpenAITextAssistant/c6-OpenAITextAssistant/MainViewModel.cs
@@ -0,0 +1,33 @@
+using System.ClientModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using OpenAI.Chat;
+
+namespace c6_OpenAITextAssistant;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty] private string _letterText;
+
+    private readonly ChatClient aiClient = new("gpt-3.5-turbo",
+        "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");
+
+    [RelayCommand]
+    private async Task FixErrorsAsync()
+    {
+        try
+        {
+            ChatCompletion completion = await aiClient.CompleteChatAsync(new List<ChatMessage>
+            {
+                new SystemChatMessage("You are an assistant correcting text"),
+                new UserChatMessage($"Fix grammar errors: {LetterText}")
+            });
+            LetterText = completion?.Content[0].Text ?? string.Empty;
+        }
+        catch (Exception ex)
+        {
+            await Shell.Current.DisplayAlert("Error",
+                ex.Message, "OK");
+        }
+    }
+}
