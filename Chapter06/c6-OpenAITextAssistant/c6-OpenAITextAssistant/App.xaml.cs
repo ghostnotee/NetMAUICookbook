a295469771b7a33825d2ed77154ae64c1ff135ff
ghostNoté <selcukbilgen@hotmail.com>
2025-05-31 15:03:04 +0300

    Add initial project structure and essential files for c6-OpenAITextAssistant

---
+++ Chapter06/c6-OpenAITextAssistant/c6-OpenAITextAssistant/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace c6_OpenAITextAssistant;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
---
+++ Chapter06/c6-OpenAITextAssistant/c6-OpenAITextAssistant/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace c6_OpenAITextAssistant;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
