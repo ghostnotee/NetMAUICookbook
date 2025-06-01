d1b6fc554e3d5c7e0a6faa6680480bb4e8433dc1
ghostNoté <selcukbilgen@hotmail.com>
2025-06-01 12:01:49 +0300

    Add OllamaSharp package and implement /ask-question endpoint

---
+++ Chapter06/c6-DeployedAiAssistant/c6-AiAssistantClient/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace c6_AiAssistantClient;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
--- Chapter06/c6-DeployedAiAssistant/c6-DeployedAiAssistant/Program.cs
+++ Chapter06/c6-DeployedAiAssistant/c6-DeployedAiAssistant/Program.cs
@@ -1,17 +1,28 @@
+using System.Text;
+using OllamaSharp;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddSingleton(_ => new OllamaApiClient(new Uri("http://localhost:11434"), "llama3.2"));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+if (app.Environment.IsDevelopment()) app.MapOpenApi();
 
 app.UseHttpsRedirection();
+
+app.MapGet("/ask-question", async (string question, OllamaApiClient ollamaClient) =>
+{
+    var chatContentBuilder = new StringBuilder();
+    await foreach (var stream in ollamaClient.GenerateAsync(question))
+        if (stream?.Response != null)
+            chatContentBuilder.Append(stream.Response);
+    return Results.Content(chatContentBuilder.ToString());
+});
 
 app.Run();
