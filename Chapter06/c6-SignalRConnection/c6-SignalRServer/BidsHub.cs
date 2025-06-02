ff440fc045b00f0c88fd6f412ee3a7576e1f5602
ghostNoté <selcukbilgen@hotmail.com>
2025-06-03 02:53:32 +0300

    Add BidsHub class for handling auction bids in SignalR server

---
+++ Chapter06/c6-SignalRConnection/c6-SignalRServer/BidsHub.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace c6_SignalRServer;
+
+public class BidsHub : Hub
+{
+    public static bool IsAuctionRunning = true;
+
+    public void AcceptBid(string winner)
+    {
+        IsAuctionRunning = false;
+    }
+}
