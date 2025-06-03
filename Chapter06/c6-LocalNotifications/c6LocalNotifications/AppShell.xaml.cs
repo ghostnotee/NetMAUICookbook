225f6c0d4359ef3d9ca49833b5880e3ed77d6aba
ghostNoté <selcukbilgen@hotmail.com>
2025-06-03 21:35:31 +0300

    Add initial project structure for c6LocalNotifications with platform-specific configurations and resources

---
+++ Chapter06/c6-LocalNotifications/c6LocalNotifications/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace c6LocalNotifications;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Chapter06/c6-LocalNotifications/c6LocalNotifications/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace c6LocalNotifications;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
