9eb3b13dc967729df1909dbdcb4b5c35c9e09b3b
ghostNoté <selcukbilgen@hotmail.com>
2025-06-03 12:29:12 +0300

    Add file upload endpoint and temporary file handling

--- Chapter06/c6-FileUploading/c6-FileUploading/Program.cs
+++ Chapter06/c6-FileUploading/c6-FileUploading/Program.cs
@@ -11,4 +11,27 @@ if (app.Environment.IsDevelopment()) app.MapOpenApi();
 
 app.UseHttpsRedirection();
 
+app.MapPost("/upload", async (HttpRequest request) =>
+{
+    var chunkNumber = int.Parse(request.Headers["Chunk-Number"]!);
+    var totalChunks = int.Parse(request.Headers["Total-Chunks"]!);
+    var fileName = request.Headers["File-Name"];
+
+    var tempFilePath = Path.Combine(Path.GetTempPath(), fileName);
+
+    await using (var fileStream = new FileStream(tempFilePath, FileMode.Append, FileAccess.Write))
+    {
+        await request.Body.CopyToAsync(fileStream);
+    }
+
+    if (chunkNumber == totalChunks - 1)
+    {
+        var finalFilePath = Path.Combine(Directory.GetCurrentDirectory(), "Uploads", fileName);
+        if (File.Exists(finalFilePath)) File.Delete(finalFilePath);
+        File.Move(tempFilePath, finalFilePath);
+    }
+
+    return Results.Ok();
+});
+
 app.Run();
