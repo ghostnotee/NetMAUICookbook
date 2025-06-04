eb1a39da435a2843d6c0968c6da0e1a4f38236a3
ghostNoté <selcukbilgen@hotmail.com>
2025-06-05 02:02:53 +0300

    Add initial project structure for c7-ConditionalCompilation with platform-specific configurations and resources

---
+++ Chapter07/c7-ConditionalCompilation/c7-ConditionalCompilation/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace c7_ConditionalCompilation;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
---
+++ Chapter07/c7-ConditionalCompilation/c7-ConditionalCompilation/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace c7_ConditionalCompilation;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
