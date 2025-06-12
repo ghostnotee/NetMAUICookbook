075d824c9f581822695e8448c4b33bfa3a057be0
ghostNoté <selcukbilgen@hotmail.com>
2025-06-12 18:13:55 +0300

    Add navigation to TestPage and simplify MainPage layout

--- Chapter08/c8-DebugVsRelease/c8-DebugVsRelease/MainPage.xaml.cs
+++ Chapter08/c8-DebugVsRelease/c8-DebugVsRelease/MainPage.xaml.cs
@@ -2,22 +2,14 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-
-	private void OnCounterClicked(object? sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    private async void OnOpenTestPageClicked(object sender, EventArgs e)
+    {
+        LoadingTimePage.StartTimer();
+        await Shell.Current.GoToAsync(nameof(TestPage), false);
+    }
 }
