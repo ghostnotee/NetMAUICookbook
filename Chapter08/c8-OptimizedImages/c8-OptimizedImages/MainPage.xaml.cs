b097fc47b926aa3a1ae5d6c83c4f75399f6ac1e1
ghostNoté <selcukbilgen@hotmail.com>
2025-06-13 16:44:37 +0300

    Update package references and add image resizing functionality in MainPage

--- Chapter08/c8-OptimizedImages/c8-OptimizedImages/MainPage.xaml.cs
+++ Chapter08/c8-OptimizedImages/c8-OptimizedImages/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace c8_DebugVsRelease;
+﻿using SkiaSharp;
+
+namespace c8_DebugVsRelease;
 
 public partial class MainPage : ContentPage
 {
@@ -11,5 +13,24 @@ public partial class MainPage : ContentPage
     {
         LoadingTimePage.StartTimer();
         await Shell.Current.GoToAsync(nameof(TestPage), false);
+    }
+
+    public Stream ResizeImage(Stream inputStream, int width, int height)
+    {
+        using SKBitmap? originalBitmap = SKBitmap.Decode(inputStream);
+
+        using SKBitmap resizedBitmap = new(width, height);
+        using SKCanvas canvas = new(resizedBitmap);
+        canvas.Clear(SKColors.Transparent);
+
+        var scale = Math.Min((float)width / originalBitmap.Width, (float)height / originalBitmap.Height);
+        SKRect destRect = new(0, 0, originalBitmap.Width * scale, originalBitmap.Height * scale);
+        canvas.DrawBitmap(originalBitmap, destRect);
+
+        MemoryStream outputStream = new();
+        resizedBitmap.Encode(outputStream, SKEncodedImageFormat.Png, 100);
+        outputStream.Seek(0, SeekOrigin.Begin);
+
+        return outputStream;
     }
 }
