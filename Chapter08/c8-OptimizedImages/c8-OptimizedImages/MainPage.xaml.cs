ef959bfe1e3797ba9f1522f0758db80f4aeb5088
ghostNoté <selcukbilgen@hotmail.com>
2025-06-13 15:54:20 +0300

    Add initial implementation of c8_DebugVsRelease application with main entry point and basic UI structure

---
+++ Chapter08/c8-OptimizedImages/c8-OptimizedImages/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace c8_DebugVsRelease;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnOpenTestPageClicked(object sender, EventArgs e)
+    {
+        LoadingTimePage.StartTimer();
+        await Shell.Current.GoToAsync(nameof(TestPage), false);
+    }
+}
