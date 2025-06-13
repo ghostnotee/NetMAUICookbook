9c8602cbbd4a5a2da8a892acfaa16e6f8bfa1dc8
ghostNoté <selcukbilgen@hotmail.com>
2025-06-13 22:56:01 +0300

    Add initial project structure and configuration for c8_AsyncLoading application

---
+++ Chapter08/c8-AsyncLoading/c8-AsyncLoading/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace c8_AsyncLoading;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
