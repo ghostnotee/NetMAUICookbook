2ca6ad5077c1bdfdbee2a355224de02ed1fd9a9b
ghostNoté <selcukbilgen@hotmail.com>
2025-06-13 11:08:02 +0300

    Enhance TestPage layout and item model with additional properties and actions

--- Chapter08/c8-SimplifiedItemTemplate/c8-SimplifiedItemTemplate/TestPage.xaml.cs
+++ Chapter08/c8-SimplifiedItemTemplate/c8-SimplifiedItemTemplate/TestPage.xaml.cs
@@ -9,13 +9,23 @@ public partial class TestPage : LoadingTimePage
         InitializeComponent();
 
         var items = new ObservableCollection<Item>();
-        for (var i = 1; i < 30; i++) items.Add(new Item(i, $"Item{i}"));
+        for (var i = 1; i < 30; i++)
+            items.Add(new Item(i,
+                $"Item{i}",
+                $"Value1_{i}",
+                $"Value2_{i}",
+                $"Value3_{i}",
+                $"Value4_{i}"));
         CollectionView.ItemsSource = items;
     }
 }
 
-public class Item(int id, string name)
+public class Item(int id, string name, string prop1, string prop2, string prop3, string prop4)
 {
     public int Id { get; set; } = id;
     public string Name { get; set; } = name;
+    public string Prop1 { get; set; } = prop1;
+    public string Prop2 { get; set; } = prop2;
+    public string Prop3 { get; set; } = prop3;
+    public string Prop4 { get; set; } = prop4;
 }
